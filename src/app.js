bd0649229b77c93f10903981a422a58658a3d682
Adrian Hernandez Barrios <ahbarrios@rappi.com>
2022-05-15 18:55:16 -0500

    Add get /contracts tests API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { Op } = require('sequelize')
 const { sequelize } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
 const app = express()
+
 app.use(bodyParser.json())
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
+
+/**
+ * @returns a list of contracts by user
+ */
+app.get('/contracts', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models')
+  // Improvement: Keep all the roles in one place to tracking all changes in Profile model type enum
+  // Just prevent to show you all when some new role gets added and this method get not updated
+  const belongsTo = ({ id, type }) => ({ client: { ClientId: id }, contractor: { ContractorId: id } })[type] || { clientId: 0 }
+  const contracts = await Contract.findAll({
+    attributes: { exclude: ['ClientId', 'ContractorId'] },
+    where: {
+      status: { [Op.ne]: 'terminated' },
+      ...belongsTo(req.profile)
+    }
+  })
+  res.json(contracts)
+})
 
 /**
  * @returns contract by id
@@ -19,4 +39,5 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   if (contractor.id !== req.profile.id) return res.status(403).end()
   res.json(contract)
 })
+
 module.exports = app
--- src/test.js
+++ src/test.js
@@ -10,3 +10,26 @@ test('get /contracts/:id owner user', async () => {
   const res = await request(app).get(`/contracts/${1}`).set('profile_id', 5)
   expect(res.statusCode).toBe(200)
 })
+
+test('get /contracts by contractor user', async () => {
+  const res = await request(app).get('/contracts').set('profile_id', 6)
+  expect(res.statusCode).toBe(200)
+  expect(res.body.map(({ id }) => id)).toEqual(expect.arrayContaining([2, 3, 8]))
+})
+
+test('get /contracts by contractor user with terminated contracts', async () => {
+  const res = await request(app).get('/contracts').set('profile_id', 5)
+  expect(res.statusCode).toBe(200)
+  expect(res.body.length).toBe(0)
+})
+
+test('get /contracts by client user', async () => {
+  const res = await request(app).get('/contracts').set('profile_id', 2)
+  expect(res.statusCode).toBe(200)
+  expect(res.body.map(({ id }) => id)).toEqual(expect.arrayContaining([3, 4]))
+})
+
+test('get /contracts by wrong user', async () => {
+  const res = await request(app).get('/contracts').set('profile_id', 9)
+  expect(res.statusCode).toBe(401)
+})
